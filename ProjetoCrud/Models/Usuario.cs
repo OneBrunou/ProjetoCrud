7ec7d8f528e28d649fb1453572a4c95545a8d8ed
OneBrunou <ms2689992@gmail.com>
2026-05-13 16:48:50 -0300

    Feat: Criando as Models

---
+++ ProjetoCrud/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjetoCrud.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        public string Nivel { get; set; } = "Operador";
+
+
+    }
+}
