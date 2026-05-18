8e06628d7ea6aae5dc17ca3983822ca463b5fce5
OneBruno <ms2689992@gmail.com>
2026-05-18 15:04:19 -0300

    Fix: arrumando a interface de produtos

--- ProjetoCrud/Repositorio/IProdutoRepositorio.cs
+++ ProjetoCrud/Repositorio/IProdutoRepositorio.cs
@@ -1,6 +1,14 @@
-﻿namespace ProjetoCrud.Repositorio
+﻿using ProjetoCrud.Models;
+
+namespace ProjetoCrud.Repositorio
 {
     public interface IProdutoRepositorio
     {
+        IEnumerable<Produto> ListarTodos();
+        Produto? ObterPorId(int id);
+        void Adicionar(Produto produto);
+        void Atualizar(Produto produto);
+        void Excluir(int id);
+
     }
 }
