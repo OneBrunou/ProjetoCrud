62221ec42c0bf0c3cc1d6f66e5e6212f1d3c60bf
OneBruno <ms2689992@gmail.com>
2026-05-18 15:48:38 -0300

    Feat: Configurando o repositorio usuario

--- ProjetoCrud/Repositorio/UsuarioRepositorio.cs
+++ ProjetoCrud/Repositorio/UsuarioRepositorio.cs
@@ -1,6 +1,38 @@
-﻿namespace ProjetoCrud.Repositorio
+﻿using MySql.Data.MySqlClient;
+using ProjetoCrud.Models;
+
+namespace ProjetoCrud.Repositorio
 {
-    public class UsuarioRepositorio
+    public class UsuarioRepositorio : IUsuarioRepositorio
     {
+        private readonly string _connectionString;
+
+        public UsuarioRepositorio(IConfiguration config) =>
+            _connectionString = config.GetConnectionString("Conexao");
+
+        public Usuario? Validar(string email, string senha)
+        {
+            using var conn = new MySqlConnection(_connectionString);
+            conn.Open();
+
+            var sql = "SELECT * FROM Usuarios WHERE Email = @e AND Senha = @s";
+            var cmd = new MySqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@e", email);
+            cmd.Parameters.AddWithValue("@s", senha);
+
+            using var reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                return new Usuario
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Nome = reader["Nome"].ToString()!,
+                    Email = reader["Email"].ToString()!,
+                    Nivel = reader["Nivel"].ToString()!
+                };
+            }
+            return null;
+        }
+
     }
 }
