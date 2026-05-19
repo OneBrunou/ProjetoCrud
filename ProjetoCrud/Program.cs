5cdca272656043dfff11f4d19010790fafbbaad9
OneBruno <ms2689992@gmail.com>
2026-05-19 06:44:15 -0300

    Feat: Criando o redirecionamento de usuario e autenticação

--- ProjetoCrud/Program.cs
+++ ProjetoCrud/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using ProjetoCrud.Repositorio;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>{
+     options.LoginPath = "/Usuario/Logar";
+     options.AccessDeniedPath = "/Usuario/AcessoNegado";
+ });
+
+builder.Services.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
+builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
