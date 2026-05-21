035caf4f2f725ac7048cd3474703fc86e051adeb
OneBrunou <ms2689992@gmail.com>
2026-05-21 16:41:16 -0300

    Feat: Criando a model Login(criar conta)

---
+++ ProjetoCrud/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Policy;
+
+namespace ProjetoCrud.Models
+{
+    public class LoginViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }=string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        public string Nivel { get; set; } = "Usuario";
+
+    }
+}
